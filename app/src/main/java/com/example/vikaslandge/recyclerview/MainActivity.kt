20620e5fd362eaf82aa7a35ee6bb5b41faf12304
vikasslandge <vikasslandge@gmail.com>
2018-08-22 16:10:33 +0530

    First commit

--- app/src/main/java/com/example/vikaslandge/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/vikaslandge/recyclerview/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.vikaslandge.recyclerview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.widget.LinearLayout
-import kotlinx.android.synthetic.main.activity_main.*
+ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var lmanager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+        var lmanager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
 
         rview.layoutManager = lmanager
         rview.adapter = MyAdapter(this)
